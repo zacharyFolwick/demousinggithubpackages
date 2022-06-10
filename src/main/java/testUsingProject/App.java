1064f59f3acb087eadc2d475be3e63e38ac36cd7
Zachary Folwick <zachary.folwick@rev.com>
2022-06-10 16:42:27 -0700

    initial commit

---
+++ src/main/java/testUsingProject/App.java
@@ -0,0 +1,13 @@
+package testUsingProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
